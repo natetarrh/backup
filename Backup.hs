e13145cba201c8294af4b0a162e41d062713b180
Nate Tarrh <nate@natetarrh.com>
2013-10-18 16:24:56 -0400

    Add best-fit packing

--- Backup.hs
+++ Backup.hs
@@ -1,6 +1,7 @@
 module Backup
 ( sort
-, write
+, writefirst
+, writebest
 , backup
 , process
 ) where
@@ -14,24 +15,32 @@ blankdisk :: Dvd
 blankdisk = (4700000000, [])
 
 backups :: [Dvd]
--- this list is INFINITE
 backups = blankdisk : backups
 
 sort :: [Album] -> [Album]
-sort albums = sortBy decreasing albums
+sort = sortBy decreasing
     where
         decreasing x y = if snd x >= snd y then LT else GT
 
 -- write album to the first backup dvd with enough space
-write :: Album -> [Dvd] -> [Dvd]
-write (a, s) ((remaining, albums):dvds) 
+writefirst :: Album -> [Dvd] -> [Dvd]
+writefirst (a, s) ((remaining, albums):dvds) 
     | s < remaining = (remaining - s, (a, s):albums):dvds
-    | otherwise = (remaining, albums):(write (a, s) dvds)
+    | otherwise = (remaining, albums):writefirst (a, s) dvds
+
+-- write album to the backup disk where it leaves the least space remaining
+writebest :: Album -> [Dvd] -> [Dvd]
+writebest (a, s) ((remaining, albums):(remaining', albums'):dvds) 
+    | null albums || s < remaining && null albums' = 
+      (remaining - s, (a, s):albums):(remaining', albums'):dvds
+    | s < remaining && s < remaining' && remaining - s < remaining' - s =
+      (remaining', albums'):writebest (a, s) ((remaining, albums):dvds)
+    | otherwise = 
+      (remaining, albums):writebest (a, s) ((remaining', albums'):dvds)
 
 backup :: [Album] -> [Dvd] -> [Dvd]
--- grab dvds until you hit one without backups 
 backup [] dvds = takeWhile ((/= []).snd) dvds
-backup ((a, s):xs) dvds = backup xs (write (a, s) dvds)
+backup ((a, s):xs) dvds = backup xs (writefirst (a, s) dvds)
 
 process :: [Album] -> [Dvd]
 process catalog = backup (sort catalog) backups
